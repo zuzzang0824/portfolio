c2310a96ef90e21820e3723c9ae85e8ca313a110
zuzzang0824 <tiny0824@naver.com>
2022-08-01 15:37:34 +0900

    0801

--- portfolioPage/script/dark.js
+++ portfolioPage/script/dark.js
@@ -1,74 +1,95 @@
-// color mode
-const dark = document.querySelector('#dark');
-const light = document.querySelector('#light');
+    // dark, light mode
+    const dark = document.querySelector('#dark');//dark btn
+    const light = document.querySelector('#light');//light btn
+    const hoverEventA = document.querySelectorAll('main .main1 #main1-container li a');
 
-const Wstroke = document.querySelectorAll('.Wstroke');
-const Wstroke1px = document.querySelectorAll('.Wstroke1px');
-const Contact = document.querySelector('.Contact');
-const before = document.querySelector('');
+    // webkit stroke
+    const Wstroke1px = document.querySelectorAll('.Wstroke1px');
+    const Wstroke = document.querySelectorAll('.Wstroke');
 
-const contactTxt = document.querySelector('.info p');
-
-const github = document.querySelector('#github_icon');
-const velog = document.querySelector('#velog_icon');
-const instagram = document.querySelector('#instagram_icon');
-const arrow = document.querySelector('#arrow');
-
-// dark mode
-
-Wstroke.forEach(Wstroke => {
-    dark.addEventListener('click', () => {
-        // body color change
-        document.body.style.backgroundColor = '#000';
-
-        // text color change
-        document.body.style.color = '#fff';
-        light.style.backgroundColor = '#fff'; 
-        dark.style.backgroundColor = '#fff'; 
+    // image change
+    const github = document.querySelector('#github_icon');
+    const velog = document.querySelector('#velog_icon');
+    const instagram = document.querySelector('#instagram_icon');
+    const arrowBox = document.querySelector('#arrowbox');
     
-        // stroke text color change
-        Wstroke.style.webkitTextStroke ='2px #fff'
-        Wstroke.style.webkitTextFillColor = 'transparent';
+    // mouse move
+    const circle = document.querySelector('.circle')
 
-        // contact (a) color change
-        Contact.style.color = '#fff';
+    const onMove = (e) =>{
+        circle.style.left = `${e.pageX-8}px`
+        circle.style.top = `${e.pageY-8}px`
+    }
 
-        // img color change
-        github.innerHTML = '<img src="./images/Wgithub.svg" alt="github" id="github_icon" width="30px">';
-        velog.innerHTML = '<img src="./images/Wvelog.svg" alt="velog" width="31px">';
-        instagram.innerHTML = '<img src="./images/Winstagram.svg" alt="instagram" id="instagram_icon" width="30px">';
+    addEventListener('mousemove', onMove);
 
-        arrow.innerHTML = '<img src="./images/arrow.svg" alt="arrow"> <div class="info"><p>이주현</p><p>수원여자대학교 시각디자인학과 미디어디자인 졸업</p><p>+82 10 8006 3955</p><p>tosuni0824@gmail.com</p>';
 
-        // footer contact color change
-        contactTxt.style.color ="red";
+    document.querySelector('#main1-container li');
+    console.log(document.querySelector('#main1-container li'));
+
+    // document.querySelector('#main1-container li').classList.add('active')
+    
+    hoverEventA.forEach(item => {
+        item.addEventListener('mouseover',(e)=>{
+            console.log(e.currentTarget);
+           e.currentTarget.parentNode.classList.add('hoverEvent')
+        })
     });
-});
 
-Wstroke1px.forEach(Wstroke1px => {
+
+    Wstroke1px.forEach(Wstroke1px => {
+        dark.addEventListener('click', () => {
+            Wstroke1px.style.webkitTextStroke ='1px #fff'
+            Wstroke1px.style.webkitTextFillColor = 'transparent';
+        });
+    });
     dark.addEventListener('click', () => {
-        Wstroke1px.style.webkitTextStroke ='1px #fff'
+        document.body.classList.add('active');
+        document.querySelector('.contact_a').classList.add('on');
+        Wstroke.forEach(Wstroke=>{
+            
+            // stroke text color change
+            Wstroke.style.webkitTextStroke ='2px #fff'
+            Wstroke.style.webkitTextFillColor = 'transparent';
+            
+            // img color change
+            github.innerHTML = '<img src="./images/Wgithub.svg" alt="github" id="github_icon" width="30px">';
+            velog.innerHTML = '<img src="./images/Wvelog.svg" alt="velog" width="31px">';
+            instagram.innerHTML = '<img src="./images/Winstagram.svg" alt="instagram" id="instagram_icon" width="30px">';   
+            
+            arrowBox.innerHTML ='<img src="./images/Warrow.svg" alt="arrow">';
+            document.querySelector('#light').src = './images/Wlight.svg'
+            document.querySelector('#dark').src = './images/Wdark.svg'
+
+             // remove .active
+            light.addEventListener('click',()=>{
+            document.body.classList.remove('active');
+            document.querySelector('.contact_a').classList.remove('on');
+            });
+        });
+        
+    });
+
+// dark -> light
+Wstroke1px.forEach(Wstroke1px => {
+    light.addEventListener('click', () => {
+        Wstroke1px.style.webkitTextStroke ='1px #000'
         Wstroke1px.style.webkitTextFillColor = 'transparent';
     });
 });
-
-
-
-
-
-
-
-
-
-//light mode
-Wstroke.forEach(Wstroke => {
-    light.addEventListener('click', () => {
-        document.body.style.backgroundColor = '#fff';
-
-        document.body.style.color = '#000';
-    
+light.addEventListener('click', () => {
+    Wstroke.forEach(Wstroke=>{
+        // stroke text color change
         Wstroke.style.webkitTextStroke ='2px #000'
         Wstroke.style.webkitTextFillColor = 'transparent';
+        
+        // img color change
+        github.innerHTML = '<img src="./images/github.svg" alt="github" id="github_icon" width="30px">';
+        velog.innerHTML = '<img src="./images/velog.svg" alt="velog" width="31px">';
+        instagram.innerHTML = '<img src="./images/instagram.svg" alt="instagram" id="instagram_icon" width="30px">';   
+        
+        arrowBox.innerHTML ='<img src="./images/arrow.svg" alt="arrow">';
+        document.querySelector('#light').src = './images/light.svg'
+        document.querySelector('#dark').src = './images/dark.svg'
     });
 });
-
